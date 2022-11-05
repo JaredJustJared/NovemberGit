fdc7521b3f7c6e091f40d24e8cc9d561dc29a915
JaredJustJared <dampirxiii@mail.ru>
2022-11-05 14:25:15 +0300

    Проверка возраста 18

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-const age = 18;
+let age;
 const gender = 'МУЖ' // жен
 
+age = +age;
 
-if (age > 18){
-    console.log("Вам больше 18 лет")
-} else {
-    console.log("Вам меньше 18 лет")
-}
+if (!isNaN(age)) {
+    if (age > 18){
+        console.log("Вам больше 18 лет")
+        } else if (age < 18){ 
+            console.log("Вам меньше 18 лет")
+        } else if (age === 18) {
+            console.log("Ура, вам 18 лет!") 
+        } else
+            console.log("Пожалуйста, укажите ваш возраст") 
+        } 
+    else {
+        console.log("Введите корректные данные")      
+    }
 
-if (gender.toLocaleLowerCase() === 'муж') {
-    console.log("Вам налево")
-}
+    
+    if (gender.toLocaleLowerCase() === 'муж') {
+        console.log("Вам налево")
+    }
 
-if (gender.toLocaleLowerCase() === 'жен') {
-    console.log("Вам направо")
-}
+    if (gender.toLocaleLowerCase() === 'жен') {
+        console.log("Вам направо")
+    }
+
+    
